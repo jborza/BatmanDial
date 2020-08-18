357ec866d046abbb8d640f87780b71de49e8f5cf
Juraj Borza <juraj.borza@nike.sk>
2020-08-18 07:40:42 +0200

    low power mode

--- BatmanDial.ino
+++ BatmanDial.ino
@@ -4,12 +4,42 @@
  * */
 #include "config.h"
 
+#define G_EVENT_VBUS_PLUGIN         _BV(0)
+#define G_EVENT_VBUS_REMOVE         _BV(1)
+#define G_EVENT_CHARGE_DONE         _BV(2)
+
+enum {
+    Q_EVENT_BMA_INT,
+    Q_EVENT_AXP_INT,
+} ;
+
+#define WATCH_FLAG_SLEEP_MODE   _BV(1)
+#define WATCH_FLAG_SLEEP_EXIT   _BV(2)
+#define WATCH_FLAG_BMA_IRQ      _BV(3)
+#define WATCH_FLAG_AXP_IRQ      _BV(4)
+
+QueueHandle_t g_event_queue_handle = NULL;
+EventGroupHandle_t g_event_group = NULL;
+EventGroupHandle_t isr_group = NULL;
+bool lenergy = false;
+
 typedef struct {
     lv_obj_t *hour;
     lv_obj_t *minute;
     lv_obj_t *second;
 } str_datetime_t;
 
+typedef enum {
+    LV_ICON_BAT_EMPTY,
+    LV_ICON_BAT_1,
+    LV_ICON_BAT_2,
+    LV_ICON_BAT_3,
+    LV_ICON_BAT_FULL,
+    LV_ICON_CHARGE,
+    LV_ICON_CALCULATION
+} lv_icon_battery_t;
+
+#define DEFAULT_SCREEN_TIMEOUT  30*1000
 
 static str_datetime_t g_data;
 TTGOClass *watch = nullptr;
@@ -17,12 +47,106 @@ PCF8563_Class *rtc;
 LV_IMG_DECLARE(batman_png);
 LV_FONT_DECLARE(morgnite_bold_64);
 
+void low_energy()
+{
+    if (watch->bl->isOn()) {
+        xEventGroupSetBits(isr_group, WATCH_FLAG_SLEEP_MODE);
+        watch->closeBL();
+        watch->stopLvglTick();
+        watch->bma->enableStepCountInterrupt(false);
+        watch->displaySleep();
+        lenergy = true;
+        // rtc_clk_cpu_freq_set(RTC_CPU_FREQ_2M);
+        setCpuFrequencyMhz(20);
+
+        Serial.println("ENTER IN LIGHT SLEEEP MODE");
+        gpio_wakeup_enable ((gpio_num_t)AXP202_INT, GPIO_INTR_LOW_LEVEL);
+        gpio_wakeup_enable ((gpio_num_t)BMA423_INT1, GPIO_INTR_HIGH_LEVEL);
+        esp_sleep_enable_gpio_wakeup ();
+        esp_light_sleep_start();
+    } else {
+        watch->startLvglTick();
+        watch->displayWakeup();
+        watch->rtc->syncToSystem();
+        updateStepCounter(watch->bma->getCounter());
+        updateBatteryLevel();
+        updateBatteryIcon(LV_ICON_CALCULATION);
+        lv_disp_trig_activity(NULL);
+        watch->openBL();
+        watch->bma->enableStepCountInterrupt();
+    }
+}
+
 void setup()
 {
     Serial.begin(115200);
+
+    //Create a program that allows the required message objects and group flags
+    g_event_queue_handle = xQueueCreate(20, sizeof(uint8_t));
+    g_event_group = xEventGroupCreate();
+    isr_group = xEventGroupCreate();
+    
     watch = TTGOClass::getWatch();
     watch->begin();
     watch->lvgl_begin();
+
+    // Turn on the IRQ used
+    watch->power->adc1Enable(AXP202_BATT_VOL_ADC1 | AXP202_BATT_CUR_ADC1 | AXP202_VBUS_VOL_ADC1 | AXP202_VBUS_CUR_ADC1, AXP202_ON);
+    watch->power->enableIRQ(AXP202_VBUS_REMOVED_IRQ | AXP202_VBUS_CONNECT_IRQ | AXP202_CHARGING_FINISHED_IRQ, AXP202_ON);
+    watch->power->clearIRQ();
+
+    // Turn off unused power
+    watch->power->setPowerOutPut(AXP202_EXTEN, AXP202_OFF);
+    watch->power->setPowerOutPut(AXP202_DCDC2, AXP202_OFF);
+    watch->power->setPowerOutPut(AXP202_LDO3, AXP202_OFF);
+    watch->power->setPowerOutPut(AXP202_LDO4, AXP202_OFF);
+
+    // Enable BMA423 interrupt ，
+    // The default interrupt configuration,
+    // you need to set the acceleration parameters, please refer to the BMA423_Accel example
+    watch->bma->attachInterrupt();
+
+    //Connection interrupted to the specified pin
+    pinMode(BMA423_INT1, INPUT);
+    attachInterrupt(BMA423_INT1, [] {
+        BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+        EventBits_t  bits = xEventGroupGetBitsFromISR(isr_group);
+        if (bits & WATCH_FLAG_SLEEP_MODE)
+        {
+            //! For quick wake up, use the group flag
+            xEventGroupSetBitsFromISR(isr_group, WATCH_FLAG_SLEEP_EXIT | WATCH_FLAG_BMA_IRQ, &xHigherPriorityTaskWoken);
+        } else
+        {
+            uint8_t data = Q_EVENT_BMA_INT;
+            xQueueSendFromISR(g_event_queue_handle, &data, &xHigherPriorityTaskWoken);
+        }
+
+        if (xHigherPriorityTaskWoken)
+        {
+            portYIELD_FROM_ISR ();
+        }
+    }, RISING);
+
+    // Connection interrupted to the specified pin
+    pinMode(AXP202_INT, INPUT);
+    attachInterrupt(AXP202_INT, [] {
+        BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+        EventBits_t  bits = xEventGroupGetBitsFromISR(isr_group);
+        if (bits & WATCH_FLAG_SLEEP_MODE)
+        {
+            //! For quick wake up, use the group flag
+            xEventGroupSetBitsFromISR(isr_group, WATCH_FLAG_SLEEP_EXIT | WATCH_FLAG_AXP_IRQ, &xHigherPriorityTaskWoken);
+        } else
+        {
+            uint8_t data = Q_EVENT_AXP_INT;
+            xQueueSendFromISR(g_event_queue_handle, &data, &xHigherPriorityTaskWoken);
+        }
+        if (xHigherPriorityTaskWoken)
+        {
+            portYIELD_FROM_ISR ();
+        }
+    }, FALLING);
+    
     rtc = watch->rtc;
 
     // Use compile time
@@ -58,6 +182,7 @@ void setup()
     lv_label_set_text(g_data.second, "00");
     lv_obj_align(g_data.second, g_data.minute, LV_ALIGN_OUT_RIGHT_MID, 9, 0);
 
+    //time task
     lv_task_create([](lv_task_t *t) {
 
         RTC_Date curr_datetime = rtc->getDateTime();
@@ -65,14 +190,124 @@ void setup()
         lv_label_set_text_fmt(g_data.minute, "%02u", curr_datetime.minute);
         lv_label_set_text_fmt(g_data.hour, "%02u", curr_datetime.hour);
 
-    }, 1000, LV_TASK_PRIO_MID, nullptr);
+    }, 1000, LV_TASK_PRIO_LOWEST, nullptr);
+
+    //battery task
+    lv_task_create([](lv_task_t *t) {
+
+       updateBatteryLevel();
+
+    }, 1000, LV_TASK_PRIO_LOWEST, nullptr);
+    
 
     // Set 20MHz operating speed to reduce power consumption
     setCpuFrequencyMhz(20);
 
 }
 
+void updateStepCounter(uint32_t counter)
+{
+    //TODO implement
+    //bar.setStepCounter(counter);
+}
+
+void updateBatteryLevel()
+{
+    TTGOClass *ttgo = TTGOClass::getWatch();
+    int p = ttgo->power->getBattPercentage();
+    //bar.updateLevel(p);
+}
+
+void updateBatteryIcon(lv_icon_battery_t icon)
+{
+    if (icon >= LV_ICON_CALCULATION) {
+        TTGOClass *ttgo = TTGOClass::getWatch();
+        int level = ttgo->power->getBattPercentage();
+        if (level > 95)icon = LV_ICON_BAT_FULL;
+        else if (level > 80)icon = LV_ICON_BAT_3;
+        else if (level > 45)icon = LV_ICON_BAT_2;
+        else if (level > 20)icon = LV_ICON_BAT_1;
+        else icon = LV_ICON_BAT_EMPTY;
+    }
+    //bar.updateBatteryIcon(icon);
+}
+
 void loop()
 {
+    bool  rlst;
+    uint8_t data;
+    //! Fast response wake-up interrupt
+    EventBits_t  bits = xEventGroupGetBits(isr_group);
+    if (bits & WATCH_FLAG_SLEEP_EXIT) {
+        if (lenergy) {
+            lenergy = false;
+            // rtc_clk_cpu_freq_set(RTC_CPU_FREQ_160M);
+            setCpuFrequencyMhz(160);
+        }
+
+        low_energy();
+
+        if (bits & WATCH_FLAG_BMA_IRQ) {
+            do {
+                rlst =  watch->bma->readInterrupt();
+            } while (!rlst);
+            xEventGroupClearBits(isr_group, WATCH_FLAG_BMA_IRQ);
+        }
+        if (bits & WATCH_FLAG_AXP_IRQ) {
+            watch->power->readIRQ();
+            watch->power->clearIRQ();
+            //TODO: Only accept axp power pek key short press
+            xEventGroupClearBits(isr_group, WATCH_FLAG_AXP_IRQ);
+        }
+        xEventGroupClearBits(isr_group, WATCH_FLAG_SLEEP_EXIT);
+        xEventGroupClearBits(isr_group, WATCH_FLAG_SLEEP_MODE);
+    }
+    if ((bits & WATCH_FLAG_SLEEP_MODE)) {
+        //! No event processing after entering the information screen
+        return;
+    }
+
+    //! Normal polling
+    if (xQueueReceive(g_event_queue_handle, &data, 5 / portTICK_RATE_MS) == pdPASS) {
+        switch (data) {
+        case Q_EVENT_BMA_INT:
+            do {
+                rlst =  watch->bma->readInterrupt();
+            } while (!rlst);
+
+            //! setp counter
+            if (watch->bma->isStepCounter()) {
+                updateStepCounter(watch->bma->getCounter());
+            }
+            break;
+        case Q_EVENT_AXP_INT:
+            watch->power->readIRQ();
+            if (watch->power->isVbusPlugInIRQ()) {
+                updateBatteryIcon(LV_ICON_CHARGE);
+            }
+            if (watch->power->isVbusRemoveIRQ()) {
+                updateBatteryIcon(LV_ICON_CALCULATION);
+            }
+            if (watch->power->isChargingDoneIRQ()) {
+                updateBatteryIcon(LV_ICON_CALCULATION);
+            }
+            if (watch->power->isPEKShortPressIRQ()) {
+                watch->power->clearIRQ();
+                low_energy();
+                return;
+            }
+            watch->power->clearIRQ();
+            break;
+
+        default:
+            break;
+        }
+
+    }
+    if (lv_disp_get_inactive_time(NULL) < DEFAULT_SCREEN_TIMEOUT) {
+        lv_task_handler();
+    } else {
+        low_energy();
+    }
     lv_task_handler();
 }
